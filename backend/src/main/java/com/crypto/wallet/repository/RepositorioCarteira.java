01df97ba8c2c157e8fc43b9926b7ef3865036453
andrelarrosa <andre.larrosa@outlook.com>
2022-07-04 21:58:37 -0300

    CRUD de Carteira

---
+++ backend/src/main/java/com/crypto/wallet/repository/RepositorioCarteira.java
@@ -0,0 +1,17 @@
+package com.crypto.wallet.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.crypto.wallet.model.Carteira;
+
+public interface RepositorioCarteira extends JpaRepository<Carteira, Long> {
+	
+	@Query(value ="select a from Carteira a where a.id = %?1% ")
+	Page<Carteira> findById(Long id, Pageable page);
+	
+	Page<Carteira> findAll(Pageable page);
+
+}
