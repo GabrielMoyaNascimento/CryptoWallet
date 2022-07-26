47c798c45e5894901413d84fec0dcfc84d526aa6
andrelarrosa <andre.larrosa@outlook.com>
2022-07-25 22:30:05 -0300

    Servicos da api

--- backend/src/main/java/com/crypto/wallet/model/Orderbook.java
+++ backend/src/main/java/com/crypto/wallet/model/Orderbook.java
@@ -1,6 +1,5 @@
 package com.crypto.wallet.model;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 import lombok.Data;
---
+++ backend/src/main/java/com/crypto/wallet/service/ServicoTrade.java
@@ -0,0 +1,17 @@
+package com.crypto.wallet.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.crypto.wallet.model.Trade;
+
+@Service
+public class ServicoTrade {
+	public Trade buscarTrades(String acronimoMoeda) {
+		String url = "https://www.mercadobitcoin.net/api/"+acronimoMoeda+"/trades";
+		RestTemplate rest = new RestTemplate();
+		Trade trade = rest.getForObject(url, Trade.class);
+		return trade;
+	}
+}
+
