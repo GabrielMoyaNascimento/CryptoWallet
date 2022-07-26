5c5ca4308d0a39b4b279741bf59ad0a483363863
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-07-26 10:28:33 -0300

    Alteração do SideBar e criação de estrutura de pastas

--- crypto-wallet/src/components/navigations/SideBar.js
+++ crypto-wallet/src/components/navigations/SideBar.js
@@ -1,15 +1,38 @@
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 
+import './SideBar.css'
+
 
 const SideBar = () => {
   return (
       <ProSidebar>
-        <Menu iconShape="square">
-          <MenuItem >Dashboard</MenuItem>
-          <SubMenu title="Components">
-            <MenuItem>Component 1</MenuItem>
-            <MenuItem>Component 2</MenuItem>
+        <Menu className='menu'>
+          <SubMenu className='submenu' title="Dashboard">
+            <MenuItem className='menuitem'>Início</MenuItem>
+            <MenuItem className='menuitem'>Relatórios</MenuItem>
+          </SubMenu>
+        </Menu>
+        <Menu className='menu' iconShape="square">
+          <SubMenu className='submenu' title="Carteira">
+            <MenuItem className='menuitem'>Minhas Carteiras</MenuItem>
+            <MenuItem className='menuitem'>Cadastrar Nova</MenuItem>
+          </SubMenu>
+        </Menu>
+        <Menu className='menu' iconShape="square">
+          <SubMenu className='submenu' title="Moedas">
+            <MenuItem className='menuitem'>Cadastrar Nova</MenuItem>
+            <MenuItem className='menuitem'>Lista</MenuItem>
+          </SubMenu>
+        </Menu>
+        <Menu className='menu' iconShape="square">
+          <SubMenu className='submenu' title="Perfil">
+            <MenuItem className='menuitem'>Meu Perfil</MenuItem>
+          </SubMenu>
+        </Menu>
+        <Menu className='menu' iconShape="square">
+          <SubMenu className='submenu' title="Configurações">
+            <MenuItem className='menuitem'>Sair</MenuItem>
           </SubMenu>
         </Menu>
       </ProSidebar>
