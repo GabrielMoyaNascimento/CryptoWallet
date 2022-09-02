8bd24f48af1d292d769dabb97fd01f6cbcc91459
andrelarrosa <andre.larrosa@outlook.com>
2022-09-02 20:11:54 -0300

    criando a tabela

--- backend/src/main/java/com/crypto/wallet/cryptoWallet/CryptoWalletApplication.java
+++ backend/src/main/java/com/crypto/wallet/cryptoWallet/CryptoWalletApplication.java
@@ -2,7 +2,9 @@ package com.crypto.wallet.cryptoWallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "com.crypto.wallet.model")
 @SpringBootApplication
 public class CryptoWalletApplication {
 
