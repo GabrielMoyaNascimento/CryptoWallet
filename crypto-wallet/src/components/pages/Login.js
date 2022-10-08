004ef6c96c5d5c03be466bfd17613592931a6b25
Mthws167 <mthws.henrique@hotmail.com>
2022-10-07 22:27:28 -0300

    organizando tela de login e registro

--- crypto-wallet/src/components/pages/Login.js
+++ crypto-wallet/src/components/pages/Login.js
@@ -1,65 +1,64 @@
-import React from 'react'
-import MenuBar from '../navigations/MenuBar'
-import Footer from '../navigations/Footer'
+import React from 'react';
+import MenuBar from '../navigations/MenuBar';
+import Footer from '../navigations/Footer';
 
 const Login = () => {
     return (
         <React.Fragment>
-            <MenuBar/>
-            <section className="content-container">
-                
-                <div className="block">
-                    <div className="row">
-                        <div className="col-left">
-                            <form id="register" action="">
-                                <h1>Cadastre-se:</h1>
-                                <fieldset>
-                                    <p id="text-cadastro">Nome:</p>
-                                    <input placeholder="Escreva seu nome" type="text" tabIndex="1" required autoFocus />
-                                </fieldset>
-                                <fieldset>
-                                    <p id="text-cadastro">E-mail:</p>
-                                    <input placeholder="Endereço de e-mail" type="email" tabIndex="2" required />
-                                </fieldset>
-                                <fieldset>
-                                    <p id="text-cadastro">Telefone:</p>
-                                    <input placeholder="Número de telefone " type="tel" tabIndex="3" required />
-                                </fieldset>
-                                <fieldset>
-                                    <p id="text-cadastro">CPF:</p>
-                                    <input placeholder="000.000.000-00" type="text" tabIndex="4" required autoFocus />
-                                </fieldset>
-                                <fieldset>
-                                    <p id="text-cadastro">Senha:</p>
-                                    <input placeholder="Senha" type="text" tabIndex="5" required />
-                                </fieldset>
-                                <fieldset>
-                                    <br></br>
-                                    <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Cadastrar</button>
-                                </fieldset>
-                            </form>
-                        </div>
-                        <div className="col-right">
-                            <form id="login" action="">
-                                <h1 id="text-login">Login:</h1>
-                                <fieldset>
-                                    <p id="text-forms">E-mail:</p>
-                                    <input placeholder="Endereço de e-mail" type="text" tabIndex="1" required autoFocus />
-                                </fieldset>
-                                <fieldset>
-                                    <p id="text-forms">Senha:</p>
-                                    <input placeholder="Senha" type="email" tabIndex="2" required />
-                                </fieldset>
-                                <fieldset>
-                                    <br></br>
-                                    <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Login</button>
-                                </fieldset>
-                            </form>
-                        </div>
+            <MenuBar />
+            <br /><br /><br /><br />
+            <div className="registro">
+                <form id="register" action="">
+                    <h1>Cadastre-se:</h1>
+                    <p>Nome:</p>
+                    <div className='col-sm-6'>
+                        <input className='text' placeholder="Escreva seu nome" type="text" tabIndex="1" required autoFocus />
+                    </div>
+
+                    <p>E-mail:</p>
+                    <input placeholder="Endereço de e-mail" type="email" tabIndex="2" required />
+
+
+                    <p>Telefone:</p>
+                    <input placeholder="Número de telefone " type="tel" tabIndex="3" required />
+
+                    <div className='col-sm-3'>
+
+                        <p>CPF:</p>
+                        <input placeholder="000.000.000-00" type="text" tabIndex="4" required autoFocus />
+
+
+                        <p>Senha:</p>
+                        <input placeholder="Senha" type="text" tabIndex="5" required />
+
+
+                        <br></br>
+                        <button className='btn btn-primary' name="submit" type="submit" id="contact-submit" data-submit="...Sending">Cadastrar</button>
+
+                    </div>
+                </form>
+            </div>
+
+            <form className="login" action="">
+                <div className='row'>
+                    <div className='col'>
+                        <h1 id="text-login">Login:</h1>
+
+                        <p id="text-forms">E-mail:</p>
+                        <input placeholder="Endereço de e-mail" type="text" tabIndex="1" required autoFocus />
+
+
+                        <p id="text-forms">Senha:</p>
+                        <input placeholder="Senha" type="email" tabIndex="2" required />
+
+
+                        <br></br>
+                        <button className='btn btn-primary' name="submit" type="submit" id="contact-submit" data-submit="...Sending">Login</button>
                     </div>
                 </div>
-            </section>
-            <Footer/>
+            </form>
+            <br/>
+            <Footer />
         </React.Fragment>
     )
 }
