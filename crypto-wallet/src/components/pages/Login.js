834fe1f5b2117257fac5facd8665cd620149ac38
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-11-25 20:06:47 -0300

    Separando login em 2 forms

--- crypto-wallet/src/components/pages/Login.js
+++ crypto-wallet/src/components/pages/Login.js
@@ -47,107 +47,111 @@ const Login = () => {
       <MenuBar />
       <div style={{ padding: "10%" }} className="col">
         <div style={{ padding: "0 25% 0 25%" }} className="row">
-        <form>
-          <div className="col-6 px-5">
-            <h1>Cadastro:</h1>
-            <p>Nome:</p>
-            <input
-              name="nome"
-              className="form-control"
-              placeholder="Escreva seu nome"
-              type="text"
-              tabIndex="1"
-              autoFocus
-              required
-              onChange={handleChange}
-            />
-            <p>E-mail:</p>
-            <input
-              name="email"
-              className="form-control"
-              placeholder="Endereço de e-mail"
-              type="email"
-              tabIndex="2"
-              required
-              onChange={handleChange}
-            />
-            <p>Telefone:</p>
-            <input
-              name="telefone"
-              className="form-control"
-              placeholder="Número de telefone "
-              type="tel"
-              tabIndex="3"
-              required
-              onChange={handleChange}
-            />
-            <p>CPF:</p>
-            <input
-              name="cpf"
-              className="form-control"
-              placeholder="000.000.000-00"
-              type="text"
-              tabIndex="4"
-              required
-              autoFocus
-              onChange={handleChange}
-            />
-            <p>Senha:</p>
-            <input
-              name="senha"
-              className="form-control"
-              placeholder="Senha"
-              type="password"
-              tabIndex="5"
-              required
-              onChange={handleChange}
-            />
-            <button
-              className="btn btn-primary form-control mt-3"
-              name="submit"
-              type="submit"
-              id="contact-submit"
-              data-submit="...Sending"
-              onClick={() => handleSubmit()}
-              value="Cadastrar"
-            >Cadastrar
-              </button>
-            </div>
-          </form>
-          <form>
-          <div className="col-6 px-5">
-            <h1 id="text-login">Login:</h1>
-            <p id="text-forms">E-mail:</p>
-            <input
-              className="form-control"
-              placeholder="Endereço de e-mail"
-              type="text"
-              tabIndex="1"
-              required
-              autoFocus
-            />
-            <p id="text-forms">Senha:</p>
-            <input
-              className="form-control"
-              placeholder="Senha"
-              type="email"
-              tabIndex="2"
-              required
-            />
-            <button
-              className="btn btn-primary form-control mt-3"
-              name="submit"
-              type="submit"
-              id="contact-submit"
-              data-submit="...Sending"
-                // disabled={true}
-            >
-                <a  className="text-decoration-none text-light" href="/dashboard">
-                Login
-              </a>
-            </button>
+        <div className="col-6 px-5">
+            <form>
+                <div>
+                  <h1>Cadastro:</h1>
+                  <p>Nome:</p>
+                  <input
+                    name="nome"
+                    className="form-control"
+                    placeholder="Escreva seu nome"
+                    type="text"
+                    tabIndex="1"
+                    autoFocus
+                    required
+                    onChange={handleChange}
+                  />
+                  <p>E-mail:</p>
+                  <input
+                    name="email"
+                    className="form-control"
+                    placeholder="Endereço de e-mail"
+                    type="email"
+                    tabIndex="2"
+                    required
+                    onChange={handleChange}
+                  />
+                  <p>Telefone:</p>
+                  <input
+                    name="telefone"
+                    className="form-control"
+                    placeholder="Número de telefone "
+                    type="tel"
+                    tabIndex="3"
+                    required
+                    onChange={handleChange}
+                  />
+                  <p>CPF:</p>
+                  <input
+                    name="cpf"
+                    className="form-control"
+                    placeholder="000.000.000-00"
+                    type="text"
+                    tabIndex="4"
+                    required
+                    autoFocus
+                    onChange={handleChange}
+                  />
+                  <p>Senha:</p>
+                  <input
+                    name="senha"
+                    className="form-control"
+                    placeholder="Senha"
+                    type="password"
+                    tabIndex="5"
+                    required
+                    onChange={handleChange}
+                  />
+                  <button
+                    className="btn btn-primary form-control mt-3"
+                    name="submit"
+                    type="submit"
+                    id="contact-submit"
+                    data-submit="...Sending"
+                    onClick={() => handleSubmit()}
+                    value="Cadastrar"
+                  >Cadastrar
+                    </button>
+                  </div>
+              </form>
           </div>
-        </form>
+          <div className="col-6 px-5">
+              <form>
+                <div>
+                  <h1 id="text-login">Login:</h1>
+                  <p id="text-forms">E-mail:</p>
+                  <input
+                    className="form-control"
+                    placeholder="Endereço de e-mail"
+                    type="text"
+                    tabIndex="1"
+                    required
+                    autoFocus
+                  />
+                  <p id="text-forms">Senha:</p>
+                  <input
+                    className="form-control"
+                    placeholder="Senha"
+                    type="email"
+                    tabIndex="2"
+                    required
+                  />
+                  <button
+                    className="btn btn-primary form-control mt-3"
+                    name="submit"
+                    type="submit"
+                    id="contact-submit"
+                    data-submit="...Sending"
+                      // disabled={true}
+                  >
+                      <a  className="text-decoration-none text-light" href="/dashboard">
+                      Login
+                    </a>
+                  </button>
+                </div>
+            </form>
+          </div>
       </div>
         
       </div>
